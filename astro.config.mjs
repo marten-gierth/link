dff03fe48c2f1d3647b74fb9128ad911ec699c15
Marten <gierth.marten@gmail.com>
2025-02-16 23:12:41 +0100

    feat: sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 
+import sitemap from '@astrojs/sitemap';
+
 export default defineConfig({
-    integrations: [react()],
+    site: "https://martengierth.de/",
+    integrations: [react(), sitemap()],
 });
