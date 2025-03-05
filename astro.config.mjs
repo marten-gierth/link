fec0ebcf689f144dfd75f4f54e66a5924e243f2f
Marten <gierth.marten@gmail.com>
2025-03-05 01:58:13 +0100

    fix: cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,21 @@
 import { defineConfig } from 'astro/config';
+import cloudflare from '@astrojs/cloudflare';
 import react from '@astrojs/react';
-
 import sitemap from '@astrojs/sitemap';
-
 import markdoc from '@astrojs/markdoc';
 import keystatic from '@keystatic/astro';
 
 export default defineConfig({
     site: "https://martengierth.de/",
-    integrations: [react(), sitemap(), markdoc(), keystatic()],
+    build: {
+        target: 'static',
+    },
+    output: "static",
+    adapter: cloudflare(),
+    integrations: [
+        react(),
+        sitemap(),
+        markdoc(),
+        keystatic(),
+    ],
 });
